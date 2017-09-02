d94461bce2bdfe18f9e1ce546ac1f6564e65f864
LauAlbert <albertlau123@gmail.com>
2017-09-02 12:45:27 -0700

    log button click

--- app/src/main/java/com/example/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/example/buttonclickcounter/MainActivity.java
@@ -2,19 +2,46 @@ package com.example.buttonclickcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private EditText userInput;
     private Button button;
     private TextView textView;
+    private int numTimesClicked = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numTimesClicked = numTimesClicked + 1;
+                String result = "\nThe button got tapped " + numTimesClicked + " time";
+                if (numTimesClicked != 1) {
+                    result += "s";
+                }
+                else {
+                    result = result.substring(1);
+                }
+                textView.append(result);
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
+
     }
 }
